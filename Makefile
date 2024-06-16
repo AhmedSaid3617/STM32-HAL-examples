e6a7900c25d52d7c8278b44e68da16d9d34ff7d6
Ahmed Said <said.ahmed3617@gmail.com>
2024-06-17 01:46:36 +0300

    Code compiles but causes error.

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
 SRC_DIR = src
 OBJS_DIR = build
 INC_DIR = inc
-LIBS_SRC = libs/STM32CubeF1/Drivers/STM32F1xx_HAL_Driver/Src
+LIBS_SRC = ./libs/STM32CubeF1/Drivers/STM32F1xx_HAL_Driver/Src
 LIBS_INCS = -I./libs/STM32CubeF1/Drivers/STM32F1xx_HAL_Driver/Inc
 LIBS_INCS += -I./libs/STM32CubeF1/Drivers/CMSIS/Device/ST/STM32F1xx/Include
 LIBS_INCS += -I./libs/STM32CubeF1/Drivers/CMSIS/Include
-INCS = -I $(INC_DIR)
+INCS = -I $(INC_DIR) $(LIBS_INCS)
 
 vpath %.c $(SRC_DIR)
 vpath %.c $(LIBS_SRC)
@@ -13,9 +13,9 @@ vpath %.o $(OBJS_DIR)
 
 PROJECT_NAME = stm32_blink
 CC = arm-none-eabi-
-CFLAGS = -mcpu=cortex-m3 -g -O0
-SRC := $(wildcard $(SRC_DIR)/*.c)
-OBJ := $(subst $(SRC_DIR)/, $(OBJS_DIR)/, $(SRC:.c=.o))
+CFLAGS = -mcpu=cortex-m3 -g -O0 -DSTM32F103xB 
+SRC := $(wildcard *.c) $(wildcard $(SRC_DIR)/*.c) 
+OBJ := $(addprefix $(OBJS_DIR)/, $(notdir $(SRC:.c=.o)) stm32f1xx_hal.o stm32f1xx_hal_cortex.o stm32f1xx_hal_rcc.o stm32f1xx_hal_gpio.o) 
 As = $(wildcard *.s)
 AsOBJ = $(As:.s=.o)
 
@@ -27,7 +27,7 @@ flash: build
 build: build_dir $(PROJECT_NAME).bin
 
 $(OBJS_DIR)/%.o: %.c
-	$(CC)gcc $< -c $(INCS) $(LIBS_INCS) -o $@   $(CFLAGS)
+	$(CC)gcc $< -c $(INCS)  -o $@   $(CFLAGS)
 
 $(OBJS_DIR)/%.o: %.s
 	$(CC)as $< -o $@ $(CFLAGS)
--- src/main.c
+++ src/main.c
@@ -1,24 +1,84 @@
-//#include "stm32f103x6.h"
-#include "GPIO.h"
+#include "stm32f1xx_hal.h"
 
-const int arr[5] = {};
+static GPIO_InitTypeDef GPIO_InitStruct;
+RCC_ClkInitTypeDef clkinitstruct = {0};
+RCC_OscInitTypeDef oscinitstruct = {0};
 
-int main()
+void SystemClock_Config(void);
+
+int main(void)
 {
-    // Enable clock for GPIOC
-    RCC_APB2ENR |= (1 << 4);
+    HAL_Init();
 
-    GPIOC_CRH |= (1 << 21);
-    GPIOC_CRH &= ~(0xD << 20);
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+
+    SystemClock_Config();
+
+    GPIO_InitStruct.Pin = GPIO_PIN_13;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
     while (1)
     {
-        for (int i = 0; i < 1000000; i++);
-        GPIOC_ODR |= (1<<13);
+        HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+        HAL_Delay(1000);
+    }
+}
 
-        for (int i = 0; i < 100000; i++);
-        GPIOC_ODR &= ~(1<<13);
+void SystemClock_Config(void)
+{
+
+    oscinitstruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    oscinitstruct.HSEState = RCC_HSE_ON;
+    oscinitstruct.LSEState = RCC_LSE_ON;
+    oscinitstruct.HSIState = RCC_HSI_ON;
+    oscinitstruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    oscinitstruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+    oscinitstruct.PLL.PLLState = RCC_PLL_ON;
+    oscinitstruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    oscinitstruct.PLL.PLLMUL = RCC_PLL_MUL16;
+    if (HAL_RCC_OscConfig(&oscinitstruct) != HAL_OK)
+    {
+        /* Initialization Error */
+        while (1)
+            ;
     }
 
-    return 0;
+    /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2
+       clocks dividers */
+    clkinitstruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+    clkinitstruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    clkinitstruct.AHBCLKDivider = RCC_SYSCLK_DIV2;
+    clkinitstruct.APB2CLKDivider = RCC_HCLK_DIV1;
+    clkinitstruct.APB1CLKDivider = RCC_HCLK_DIV2;
+    if (HAL_RCC_ClockConfig(&clkinitstruct, FLASH_LATENCY_2) != HAL_OK)
+    {
+        /* Initialization Error */
+        while (1)
+            ;
+    }
 }
+
+#ifdef USE_FULL_ASSERT
+
+/**
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+    /* User can add his own implementation to report the file name and line number,
+       ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+    printf("Wrong parameters value: file %s on line %lu\r\n", file, line);
+
+    /* Infinite loop */
+    while (1)
+    {
+    }
+}
+#endif
